f9affd029f2d0b88becffa7f823b2ea0acaee04b
Terrible-creeper <creeper5620@gmail.com>
2021-05-25 21:00:35 +0800

    upload

---
+++ WindowsApplication2/Form1.Designer.vb
@@ -0,0 +1,359 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.DataTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ComputerTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.CheckBox8 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox7 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox6 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox5 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox4 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'DataTimer
+        '
+        '
+        'SerialPort1
+        '
+        Me.SerialPort1.BaudRate = 38400
+        Me.SerialPort1.PortName = "COM3"
+        Me.SerialPort1.ReadTimeout = 500
+        Me.SerialPort1.ReceivedBytesThreshold = 10
+        Me.SerialPort1.WriteTimeout = 500
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("微軟正黑體", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(189, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(319, 35)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = "Current Time:"
+        '
+        'ComputerTimer
+        '
+        Me.ComputerTimer.Enabled = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.ComboBox1)
+        Me.GroupBox1.Font = New System.Drawing.Font("微軟正黑體", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(45, 90)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(339, 141)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Device Connect"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Red
+        Me.Label2.Location = New System.Drawing.Point(130, 97)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(62, 21)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Offline"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 97)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(113, 21)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Device Status"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(215, 29)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 28)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "CLOSE"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(134, 29)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 28)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "OPEN"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(6, 28)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 29)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.GroupBox2.Controls.Add(Me.Button3)
+        Me.GroupBox2.Controls.Add(Me.TextBox2)
+        Me.GroupBox2.Font = New System.Drawing.Font("微軟正黑體", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.GroupBox2.Location = New System.Drawing.Point(402, 90)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(274, 141)
+        Me.GroupBox2.TabIndex = 2
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Device Password"
+        '
+        'Button3
+        '
+        Me.Button3.Enabled = False
+        Me.Button3.Location = New System.Drawing.Point(121, 34)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(147, 84)
+        Me.Button3.TabIndex = 1
+        Me.Button3.Text = "Write" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "EEPROM" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Enabled = False
+        Me.TextBox2.Location = New System.Drawing.Point(15, 57)
+        Me.TextBox2.MaxLength = 4
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 33)
+        Me.TextBox2.TabIndex = 0
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.GroupBox3.Controls.Add(Me.Button5)
+        Me.GroupBox3.Controls.Add(Me.Button4)
+        Me.GroupBox3.Controls.Add(Me.CheckBox8)
+        Me.GroupBox3.Controls.Add(Me.CheckBox7)
+        Me.GroupBox3.Controls.Add(Me.CheckBox6)
+        Me.GroupBox3.Controls.Add(Me.CheckBox5)
+        Me.GroupBox3.Controls.Add(Me.CheckBox4)
+        Me.GroupBox3.Controls.Add(Me.CheckBox3)
+        Me.GroupBox3.Controls.Add(Me.CheckBox2)
+        Me.GroupBox3.Controls.Add(Me.CheckBox1)
+        Me.GroupBox3.Font = New System.Drawing.Font("微軟正黑體", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.GroupBox3.Location = New System.Drawing.Point(45, 246)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(631, 100)
+        Me.GroupBox3.TabIndex = 3
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "LED Control"
+        '
+        'Button5
+        '
+        Me.Button5.Enabled = False
+        Me.Button5.Location = New System.Drawing.Point(349, 66)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(123, 28)
+        Me.Button5.TabIndex = 9
+        Me.Button5.Text = "All Lights OFF"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Enabled = False
+        Me.Button4.Location = New System.Drawing.Point(134, 66)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(129, 28)
+        Me.Button4.TabIndex = 8
+        Me.Button4.Text = "All Lights ON"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'CheckBox8
+        '
+        Me.CheckBox8.AutoSize = True
+        Me.CheckBox8.Enabled = False
+        Me.CheckBox8.Location = New System.Drawing.Point(551, 28)
+        Me.CheckBox8.Name = "CheckBox8"
+        Me.CheckBox8.Size = New System.Drawing.Size(68, 25)
+        Me.CheckBox8.TabIndex = 7
+        Me.CheckBox8.Text = "LED7"
+        Me.CheckBox8.UseVisualStyleBackColor = True
+        '
+        'CheckBox7
+        '
+        Me.CheckBox7.AutoSize = True
+        Me.CheckBox7.Enabled = False
+        Me.CheckBox7.Location = New System.Drawing.Point(474, 28)
+        Me.CheckBox7.Name = "CheckBox7"
+        Me.CheckBox7.Size = New System.Drawing.Size(68, 25)
+        Me.CheckBox7.TabIndex = 6
+        Me.CheckBox7.Text = "LED6"
+        Me.CheckBox7.UseVisualStyleBackColor = True
+        '
+        'CheckBox6
+        '
+        Me.CheckBox6.AutoSize = True
+        Me.CheckBox6.Enabled = False
+        Me.CheckBox6.Location = New System.Drawing.Point(397, 28)
+        Me.CheckBox6.Name = "CheckBox6"
+        Me.CheckBox6.Size = New System.Drawing.Size(68, 25)
+        Me.CheckBox6.TabIndex = 5
+        Me.CheckBox6.Text = "LED5"
+        Me.CheckBox6.UseVisualStyleBackColor = True
+        '
+        'CheckBox5
+        '
+        Me.CheckBox5.AutoSize = True
+        Me.CheckBox5.Enabled = False
+        Me.CheckBox5.Location = New System.Drawing.Point(320, 28)
+        Me.CheckBox5.Name = "CheckBox5"
+        Me.CheckBox5.Size = New System.Drawing.Size(68, 25)
+        Me.CheckBox5.TabIndex = 4
+        Me.CheckBox5.Text = "LED4"
+        Me.CheckBox5.UseVisualStyleBackColor = True
+        '
+        'CheckBox4
+        '
+        Me.CheckBox4.AutoSize = True
+        Me.CheckBox4.Enabled = False
+        Me.CheckBox4.Location = New System.Drawing.Point(243, 28)
+        Me.CheckBox4.Name = "CheckBox4"
+        Me.CheckBox4.Size = New System.Drawing.Size(68, 25)
+        Me.CheckBox4.TabIndex = 3
+        Me.CheckBox4.Text = "LED3"
+        Me.CheckBox4.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Enabled = False
+        Me.CheckBox3.Location = New System.Drawing.Point(166, 28)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(68, 25)
+        Me.CheckBox3.TabIndex = 2
+        Me.CheckBox3.Text = "LED2"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Enabled = False
+        Me.CheckBox2.Location = New System.Drawing.Point(89, 28)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(68, 25)
+        Me.CheckBox2.TabIndex = 1
+        Me.CheckBox2.Text = "LED1"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Enabled = False
+        Me.CheckBox1.Location = New System.Drawing.Point(12, 28)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(68, 25)
+        Me.CheckBox1.TabIndex = 0
+        Me.CheckBox1.Text = "LED0"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Font = New System.Drawing.Font("微軟正黑體", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Button6.Location = New System.Drawing.Point(279, 352)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(129, 28)
+        Me.Button6.TabIndex = 9
+        Me.Button6.Text = "Exit"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(751, 389)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "109學年度 工業類科學生技藝競賽 電腦修護職種 第二站 崗位號碼 : 44"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents DataTimer As System.Windows.Forms.Timer
+    Friend WithEvents SerialPort1 As System.IO.Ports.SerialPort
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents ComputerTimer As System.Windows.Forms.Timer
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
+    Friend WithEvents CheckBox8 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox7 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox6 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox5 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox4 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox3 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox2 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+End Class
---
+++ WindowsApplication2/Form1.vb
@@ -0,0 +1,282 @@
+﻿Public Class Form1
+#Region "Dim"
+    Dim link_time_now, link_time_last As DateTime
+    Dim data(10), data2(10) As Byte
+    Dim VR As Integer
+    Dim passwd16, passwd As Integer
+    Dim LED_value As Integer
+#End Region
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ComboBox1.Items.Clear()
+        For Each sp As String In My.Computer.Ports.SerialPortNames
+            ComboBox1.Items.Add(sp)
+        Next
+        ComboBox1.SelectedIndex = 0
+        DataTimer.Enabled = True
+    End Sub
+
+    Private Sub SerialPort1_DataReceived(ByVal sender As System.Object, ByVal e As System.IO.Ports.SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
+        Dim buff(SerialPort1.BytesToRead - 1) As Byte
+        SerialPort1.Read(buff, 0, buff.Length)
+        If buff.Length >= 9 Then
+            If (buff(0) = &H3A) Then
+
+            End If
+        End If
+    End Sub
+
+    Private Sub DataTimer_Tick(sender As Object, e As EventArgs) Handles DataTimer.Tick
+
+        Try
+            data(0) = &H3A
+            data(1) = &H0
+            data(2) = LED_value
+            data(3) = &H0
+            data(4) = &H0
+            data(5) = &H0
+            data(6) = &H0
+            data(7) = &H0
+            data(8) = &H0
+            data(9) = &H0
+            SerialPort1.Write(data, 0, 10)
+        Catch ex As Exception
+            Button2.PerformClick()
+        End Try
+    End Sub
+
+
+    Private Sub relink()
+        Try
+            If SerialPort1.IsOpen = True Then
+                SerialPort1.Close()
+            End If
+            SerialPort1.Open()
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+
+    Function IntToBCD(value As Integer)
+        Dim num As String = value
+
+        If num.Length = 1 Then
+            num = "0" & num
+        End If
+
+        Dim data1 As String = ""
+
+        Dim bcd(9) As String
+        bcd(0) = "0000"
+        bcd(1) = "1000"
+        bcd(2) = "0100"
+        bcd(3) = "1100"
+        bcd(4) = "0010"
+        bcd(5) = "1010"
+        bcd(6) = "0110"
+        bcd(7) = "1110"
+        bcd(8) = "0001"
+        bcd(9) = "1001"
+
+        For i As Integer = 1 To num.Length
+            data1 = bcd(num.Substring(i - 1, 1)) & data1
+        Next
+
+        Dim require As Integer = Convert.ToUInt64(data1, 2)
+        Return require
+    End Function
+
+    Private Sub ComputerTimer_Tick(sender As Object, e As EventArgs) Handles ComputerTimer.Tick
+        TextBox1.Text = "Current Time : " & DateTime.Now.ToString("hh:mm:ss")
+    End Sub
+
+    Private Sub ComboBox1_Click(sender As Object, e As EventArgs) Handles ComboBox1.Click
+        ComboBox1.Items.Clear()
+        For Each sp As String In My.Computer.Ports.SerialPortNames
+            ComboBox1.Items.Add(sp)
+        Next
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        SerialPort1.PortName = ComboBox1.SelectedItem
+
+        Try
+            SerialPort1.Open()
+            Button1.Enabled = False
+            CheckBox1.Enabled = True
+            CheckBox2.Enabled = True
+            CheckBox3.Enabled = True
+            CheckBox4.Enabled = True
+            CheckBox5.Enabled = True
+            CheckBox6.Enabled = True
+            CheckBox7.Enabled = True
+            CheckBox8.Enabled = True
+            TextBox2.Enabled = True
+            Button3.Enabled = True
+            Button4.Enabled = True
+            Button5.Enabled = True
+            Label2.Text = "Online"
+            Label2.BackColor = Color.Green
+        Catch ex As Exception
+            SerialPort1.Close()
+            Button1.Enabled = True
+            CheckBox1.Enabled = False
+            CheckBox2.Enabled = False
+            CheckBox3.Enabled = False
+            CheckBox4.Enabled = False
+            CheckBox5.Enabled = False
+            CheckBox6.Enabled = False
+            CheckBox7.Enabled = False
+            CheckBox8.Enabled = False
+            TextBox2.Enabled = False
+            Button3.Enabled = False
+            Button4.Enabled = False
+            Button5.Enabled = False
+            Label2.Text = "Offline"
+            Label2.BackColor = Color.Red
+        End Try
+        
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        SerialPort1.Close()
+        Button1.Enabled = True
+        CheckBox1.Enabled = False
+        CheckBox2.Enabled = False
+        CheckBox3.Enabled = False
+        CheckBox4.Enabled = False
+        CheckBox5.Enabled = False
+        CheckBox6.Enabled = False
+        CheckBox7.Enabled = False
+        CheckBox8.Enabled = False
+        TextBox2.Enabled = False
+        Button3.Enabled = False
+        Button4.Enabled = False
+        Button5.Enabled = False
+        Label2.Text = "Offline"
+        Label2.BackColor = Color.Red
+    End Sub
+
+    Private Sub TextBox2_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles TextBox2.KeyPress
+        If e.KeyChar = Chr(48) Or e.KeyChar = Chr(49) Or e.KeyChar = Chr(50) Or e.KeyChar = Chr(51) Or e.KeyChar = Chr(52) Or e.KeyChar = Chr(53) Or e.KeyChar = Chr(54) Or e.KeyChar = Chr(55) Or e.KeyChar = Chr(56) Or e.KeyChar = Chr(57) Or e.KeyChar = Chr(13) Or e.KeyChar = Chr(8) Then
+            e.Handled = False
+        Else
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If TextBox2.TextLength < 4 Then
+            TextBox2.Text = ""
+        ElseIf TextBox2.TextLength = 4 Then
+            Dim eeprom As String = TextBox2.Text
+            Try
+                data2(0) = &H3A
+                data2(1) = &H3
+                data2(2) = eeprom.Substring(0, 2)
+                data2(3) = eeprom.Substring(2, 2)
+                data2(4) = IntToBCD(eeprom.Substring(2, 2))
+                data2(5) = &H0
+                data2(6) = &H0
+                data2(7) = &H0
+                data2(8) = &H0
+                data2(9) = &H0
+                SerialPort1.Write(data2, 0, 10)
+            Catch ex As Exception
+                MsgBox(ex.ToString)
+            End Try
+        End If
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.CheckState = CheckState.Checked Then
+            LED_value = LED_value + &H1
+        ElseIf CheckBox1.CheckState = CheckState.Unchecked Then
+            LED_value = LED_value - &H1
+        End If
+    End Sub
+
+    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
+        If CheckBox2.CheckState = CheckState.Checked Then
+            LED_value = LED_value + &H2
+        ElseIf CheckBox2.CheckState = CheckState.Unchecked Then
+            LED_value = LED_value - &H2
+        End If
+    End Sub
+
+    Private Sub CheckBox3_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox3.CheckedChanged
+        If CheckBox3.CheckState = CheckState.Checked Then
+            LED_value = LED_value + &H4
+        ElseIf CheckBox3.CheckState = CheckState.Unchecked Then
+            LED_value = LED_value - &H4
+        End If
+    End Sub
+
+    Private Sub CheckBox4_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox4.CheckedChanged
+        If CheckBox4.CheckState = CheckState.Checked Then
+            LED_value = LED_value + &H8
+        ElseIf CheckBox4.CheckState = CheckState.Unchecked Then
+            LED_value = LED_value - &H8
+        End If
+    End Sub
+
+    Private Sub CheckBox5_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox5.CheckedChanged
+        If CheckBox5.CheckState = CheckState.Checked Then
+            LED_value = LED_value + &H10
+        ElseIf CheckBox5.CheckState = CheckState.Unchecked Then
+            LED_value = LED_value - &H10
+        End If
+    End Sub
+
+    Private Sub CheckBox6_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox6.CheckedChanged
+        If CheckBox6.CheckState = CheckState.Checked Then
+            LED_value = LED_value + &H20
+        ElseIf CheckBox6.CheckState = CheckState.Unchecked Then
+            LED_value = LED_value - &H20
+        End If
+    End Sub
+
+    Private Sub CheckBox7_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox7.CheckedChanged
+        If CheckBox7.CheckState = CheckState.Checked Then
+            LED_value = LED_value + &H40
+        ElseIf CheckBox7.CheckState = CheckState.Unchecked Then
+            LED_value = LED_value - &H40
+        End If
+    End Sub
+
+    Private Sub CheckBox8_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox8.CheckedChanged
+        If CheckBox8.CheckState = CheckState.Checked Then
+            LED_value = LED_value + &H80
+        ElseIf CheckBox8.CheckState = CheckState.Unchecked Then
+            LED_value = LED_value - &H80
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        CheckBox1.Checked = True
+        CheckBox2.Checked = True
+        CheckBox3.Checked = True
+        CheckBox4.Checked = True
+        CheckBox5.Checked = True
+        CheckBox6.Checked = True
+        CheckBox7.Checked = True
+        CheckBox8.Checked = True
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        CheckBox1.Checked = False
+        CheckBox2.Checked = False
+        CheckBox3.Checked = False
+        CheckBox4.Checked = False
+        CheckBox5.Checked = False
+        CheckBox6.Checked = False
+        CheckBox7.Checked = False
+        CheckBox8.Checked = False
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Me.Close()
+    End Sub
+End Class
